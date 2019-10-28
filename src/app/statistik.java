ec8436a1cc6363a2e6856d25b54ef50ac6b5b48f
Andreasnoer <56431623+Andreasnoer@users.noreply.github.com>
2019-10-28 09:15:16 +0100

    statistik

---
+++ src/app/statistik.java
@@ -0,0 +1,30 @@
+package app;
+
+import java.util.Scanner;
+/**
+ *
+ * @author andreas
+ */
+
+public class statistik {
+
+int[] popArr = new int[15];
+    
+ int one = 0;
+ int two = 0;
+ int three = 0;
+ int four = 0;
+ int five = 0;
+ int six = 0;
+ int seven = 0;
+ int eight = 0;
+ int nine = 0;
+ int ten = 0;
+ int eleven = 0;
+ int twelve = 0;
+ int thirteen = 0;
+ int fourteem = 0;
+ 
+}
+
+  
